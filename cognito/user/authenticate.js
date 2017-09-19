1f88ffa036ef6bb8c951297bb508c3b8f83151b2
RafPe <rafal@pieniazek.nl>
2017-09-19 11:02:19 +0200

    Register new user works

---
+++ cognito/user/authenticate.js
@@ -0,0 +1,57 @@
+'use strict';
+
+var xSharedFunctions = require('../xRes/shared/xSharedFunctions');
+var xCognitoUserPoolManager = require('../xRes/xCognitoUserPoolManager');
+
+
+const uuid      = require('uuid');
+const component  = 'cognito'
+
+var xSharedFnc = new xSharedFunctions('cognito');
+
+module.exports.authenticate = (event, context, callback) => {
+    const uniqueId      = uuid.v1();
+    
+    var xCognitoUserPoolMgr = new xCognitoUserPoolManager(uniqueId,callback);
+  
+    xSharedFnc.logmsg(uniqueId,'info','Starting execution');
+    xSharedFnc.logmsg(uniqueId,'info',`${JSON.stringify(event)}`);
+   
+    if ( !xSharedFnc.isDef(event.body) )
+      { 
+        xSharedFnc.logmsg(uniqueId,'error','Missing body information (EC.001)');
+  
+        let errorData = {
+          code: "EC.001",
+          data: {
+            message: "Missing body"
+          }
+        }
+  
+        callback(null,xSharedFnc.generateErrorResponse(errorData)); 
+  
+      }
+  
+  
+    var jsonBody = JSON.parse(event.body);
+  
+    if ( !xSharedFnc.isDef(jsonBody.username) || !xSharedFnc.isDef(jsonBody.email) || !xSharedFnc.isDef(jsonBody.mobile) || !xSharedFnc.isDef(jsonBody.password) )
+    {
+      xSharedFnc.logmsg(uniqueId,'error','Missing required parameters in body (EC.002)');
+      
+      let errorData = {
+        code: "EC.002",
+        data: {
+          message: "Missing required parameters in body"
+        }
+      }
+  
+      callback(null,xSharedFnc.generateErrorResponse(errorData)); 
+    }
+  
+    xSharedFnc.logmsg(uniqueId,'info','All required parameters received');
+    xSharedFnc.logmsg(uniqueId,'info','Calling createPlatformEndpoint...');
+  
+    xCognitoUserPoolMgr.createNew(jsonBody.username,jsonBody.email,jsonBody.mobile,jsonBody.password)           
+  }
+  
--- cognito/xRes/xCognitoUserPoolManager.js
+++ cognito/xRes/xCognitoUserPoolManager.js
@@ -3,13 +3,12 @@
 var AWS = require('aws-sdk');
 const uuid = require('uuid');
 var CognitoSDK = require('amazon-cognito-identity-js');
-var functionsCommon = require('./common.js');
+var xSharedFunctions = require('./shared/xSharedFunctions');
 
 AWS.CognitoIdentityServiceProvider.AuthenticationDetails = CognitoSDK.AuthenticationDetails;
 AWS.CognitoIdentityServiceProvider.CognitoUserPool = CognitoSDK.CognitoUserPool;
 AWS.CognitoIdentityServiceProvider.CognitoUser = CognitoSDK.CognitoUser;
 AWS.CognitoIdentityServiceProvider.CognitoUserAttribute = CognitoSDK.CognitoUserAttribute;
-
 
 
 AWS.config.region = process.env.REGION;
@@ -21,15 +20,103 @@ var poolData = {
 
 
 class xCognitoUserPoolManager {  
-    createNew(username, password, userAttributes) {
+    constructor(uuid,callback,disableLog){
+        
+        this.userPool   = new AWS.CognitoIdentityServiceProvider.CognitoUserPool(poolData);
+        this.callback   = callback;
+        this.component  = 'cognito';
 
-        const component = 'RegisterCognitoUser'
+        this.xSharedFnc = new xSharedFunctions(this.component,null,(disableLog === null || disableLog === undefined ) ? null : disableLog);  
+        this.uuid       = uuid;
 
-        var userPool = new AWS.CognitoIdentityServiceProvider.CognitoUserPool(poolData);
 
+        this.sns = new AWS.SNS({
+            apiVersion: '2010-03-31',
+            region: process.env.REGION
+        });
+
+    }
+
+    authenticate(usrName,usrPass){
+        var that = this;
+        let res  = null;
+
+        
+        const user = new AWS.CognitoIdentityServiceProvider.CognitoUser({ Username: usrName, Pool: that.userPool });
+        
+        const authenticationData    = { Username: usrName, Password: usrPass };
+        const authenticationDetails = new AWS.CognitoIdentityServiceProvider.AuthenticationDetails(authenticationData);
+        
+        user.authenticateUser(authenticationDetails, {
+            onSuccess: result => {
+                that.xSharedFnc.logmsg(that.uuid,'info','authenticated cognito user');
+                that.xSharedFnc.logmsg(that.uuid,'info',`${JSON.stringify(result)}`);
+
+                res = that.xSharedFnc.generateSuccessResponse(result);
+
+                that.xSharedFnc.logmsg(that.uuid,'info',`${JSON.stringify(res)}`);
+
+                that.callback(null,res);            
+
+                return res;
+            },
+            onFailure: error => {
+                that.xSharedFnc.logmsg(that.uuid,'error','Failed to authenticate cognito user (EC.007)');
+                that.xSharedFnc.logmsg(that.uuid,'error',`${JSON.stringify(error)}`);
+
+                let errorData = {
+                    code: "EC.006",
+                    data: error
+                }
+
+                res = that.xSharedFnc.generateErrorResponse(errorData,errorData.data.statusCode);
+
+                that.callback(null,res);
+
+                return res;
+            },
+            newPasswordRequired: () => {},  // no-op
+            mfaRequired: () => {},  // no-op
+            customChallenge: () => {} // no-op
+        });
+                    
+    }
+
+    createNew(username, email, mobile ,password ,xtraUserAttributes) {
+        var that = this;
+        let res  = null;
+
+        var userPool      = new AWS.CognitoIdentityServiceProvider.CognitoUserPool(poolData);
         var attributeList = [];
         
-        userAttributes.forEach(function(entry) {
+        if(xtraUserAttributes != true ){
+            xtraUserAttributes = [];
+        }
+
+        var attrEmail = {
+            name : 'email',
+            value : email
+        };
+    
+        xtraUserAttributes.push(attrEmail);
+    
+        var attrMobile = {
+            name : 'phone_number',
+            value : mobile
+        };
+    
+        xtraUserAttributes.push(attrMobile);
+    
+        var attrUsername = {
+            name : 'nickname',
+            value : username
+        };
+    
+        xtraUserAttributes.push(attrUsername);
+
+
+
+        xtraUserAttributes.forEach(function(entry) {
                 
             var tmpAttr = {
                 Name :  entry.name,
@@ -43,20 +130,37 @@ class xCognitoUserPoolManager {
         
         }, this);
 
+        
         userPool.signUp(username, password, attributeList, null, function(err, result){
             if (err) {
-                functionsCommon.logmsg(component,uniqueId,'error', JSON.stringify(err) )
-              
+
+                that.xSharedFnc.logmsg(that.uuid,'error','Failed to create cognito user (EC.006)');
+                that.xSharedFnc.logmsg(that.uuid,'error',`${JSON.stringify(err)}`);
+
                 let errorData = {
-                    code: "EC.003",
+                    code: "EC.006",
                     data: err
                 }
-    
-                callback(null,functionsCommon.generateErrorResponse(null,errorData)); 
+
+                res = that.xSharedFnc.generateErrorResponse(errorData,errorData.data.statusCode);
+
+                that.callback(null,res);
+
+                return res;
     
             } else {
     
-                callback(null,functionsCommon.generateSuccessResponse(201,result)); 
+                that.xSharedFnc.logmsg(that.uuid,'info','Created cognito user');
+                that.xSharedFnc.logmsg(that.uuid,'info',`${JSON.stringify(result)}`);
+
+
+                res = that.xSharedFnc.generateSuccessResponse(result,201);
+
+                that.xSharedFnc.logmsg(that.uuid,'info',`${JSON.stringify(res)}`);
+
+                that.callback(null,res);            
+
+                return res;
             }
           });
     }
